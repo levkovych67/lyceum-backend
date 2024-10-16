5ed6573ba971464c9c3fba0844ff9332a5160f93
Golubenko Nice <yo@mama.com>
2024-10-16 11:08:38 -0700

    request mapping and git ignore

--- src/main/java/com/lyceum/controller/StatusController.java
+++ src/main/java/com/lyceum/controller/StatusController.java
@@ -1,11 +1,12 @@
 package com.lyceum.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1/status")
+@RestController
+@RequestMapping("/api/v1/status")
 public class StatusController {
-
 
     @GetMapping
     public String status(){
